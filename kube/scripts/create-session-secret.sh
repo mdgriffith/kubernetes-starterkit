ae32f60abdcbe2a8780a8c38a41c003dd12a9add
Matthew Griffith <mdg.griffith@gmail.com>
2016-08-05 11:54:29 -0400

    Prevent erring when generating session secret

--- kube/scripts/create-session-secret.sh
+++ kube/scripts/create-session-secret.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Creates a session secret
-SECRET=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+SECRET=$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)
 
 eval "kubectl create secret generic session-secret --from-literal=session-secret=$SECRET"
